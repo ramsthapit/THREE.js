11554f94a26ce91d798f1a7e9bf6ef940577b1cc
Ram Sthapit <ramsthapit50@gmail.com>
2021-10-14 16:53:48 +0545

    Orbit Control

--- animation/src/script.js
+++ animation/src/script.js
@@ -1,9 +1,26 @@
 import './style.css'
 import * as THREE from 'three'
-import gsap from 'gsap'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Cursor
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
 
 // Scene
 const scene = new THREE.Scene()
@@ -14,48 +31,48 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
+
 
 // Axeshelper
 // const AxesHelper = new THREE.AxesHelper(2)
 // scene.add(AxesHelper)
 
 // Camera
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
 const aspectRatio = sizes.width/sizes.height
-const camera = new THREE.OrthographicCamera(-1*aspectRatio, 1*aspectRatio, 1, -1, 0.1, 100)
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+// const camera = new THREE.OrthographicCamera(-1*aspectRatio, 1*aspectRatio, 1, -1, 0.1, 100)
 camera.position.z = 3
-camera.position.y = 1
-camera.position.x = 1
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+// controls.target.y = 2
+controls.enableDamping = true
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
 
 // clock
-// const clock = new THREE.Clock()
+const clock = new THREE.Clock()
 
 // gsap.to(mesh.position, { duration: 1, delay: 1, x: 1})
 
 //Animations
 const tick = () => {
 
-    // // clock
-    // const elapseTime = clock.getElapsedTime()
-
-    // // Update objects
-    mesh.rotation.y += 0.01
-    // camera.position.y = Math.cos(elapseTime)
-    // // camera.position.x = Math.sin(elapseTime)
-    camera.lookAt(mesh.position)
+    // clock
+    const elapseTime = clock.getElapsedTime()
+    
+    // update control
+    controls.update()
+    
+    // Update camera
+    camera.position.y = cursor.y * 3
+    // camera.position.x = cursor.x * 3
+    // camera.lookAt(new THREE.Vector3())
     
     // render 
     renderer.render(scene, camera)
