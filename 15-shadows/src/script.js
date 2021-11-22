68d0e9ff1ca4e5a6dd50e01dce957ca526831add
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-22 15:55:13 +0545

    directionalLight-Shadow

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -32,6 +32,21 @@ gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
 
+directionalLight.castShadow = true
+
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.camera.top = 2
+directionalLight.shadow.camera.right = 2
+directionalLight.shadow.camera.bottom = -2 
+directionalLight.shadow.camera.left = -2
+directionalLight.shadow.camera.near = 2
+directionalLight.shadow.camera.far = 6
+
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+scene.add(directionalLightCameraHelper)
+
+
 /**
  * Materials
  */
@@ -47,11 +62,14 @@ const sphere = new THREE.Mesh(
     new THREE.SphereBufferGeometry(0.5, 32, 32),
     material
 )
+sphere.castShadow = true
 
 const plane = new THREE.Mesh(
     new THREE.PlaneBufferGeometry(5, 5),
     material
 )
+plane.receiveShadow = true
+
 plane.rotation.x = - Math.PI * 0.5
 plane.position.y = - 0.5
 
@@ -102,6 +120,9 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFShadowMap
 
 /**
  * Animate
