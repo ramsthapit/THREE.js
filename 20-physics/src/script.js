cc53c54e93bb0e49faef9578af5dd0f52394f503
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-24 20:03:40 +0545

    applyForce - physics

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -62,6 +62,7 @@ const sphereBody = new CANNON.Body({
     position: new CANNON.Vec3(0, 3, 0),
     shape: sphereShape,
 })
+sphereBody.applyLocalForce(new CANNON.Vec3(150, 0, 0), new CANNON.Vec3(0, 0, 0))
 world.addBody(sphereBody)
 
 // Floor
@@ -182,6 +183,9 @@ const tick = () =>
     oldElapsedTime = elapsedTime
 
     // Update physics world
+
+    sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position)
+    
     world.step(1 / 60, deltaTime, 3)
 
     sphere.position.copy(sphereBody.position)
