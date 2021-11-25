5f8dede96b4d1825df052783880ac5a31d495d21
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-25 11:15:09 +0545

    use of draco

--- 21-imported-models/src/script.js
+++ 21-imported-models/src/script.js
@@ -3,6 +3,7 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as dat from 'dat.gui'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 /**
  * Base
@@ -19,10 +20,14 @@ const scene = new THREE.Scene()
 /**
  * Models
  */
+const dracoLoader = new DRACOLoader()
+dracoLoader.setDecoderPath('/draco/')
+
 const gltfLoader = new GLTFLoader()
+gltfLoader.setDRACOLoader(dracoLoader)
 
 gltfLoader.load(
-    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/Duck/glTF-Draco/Duck.gltf',
     (gltf) => {
         // const children = [...gltf.scene.children]
         // for (const child of children)
@@ -32,7 +37,6 @@ gltfLoader.load(
         scene.add(gltf.scene)
     },
 )
-
 
 /**
  * Floor
