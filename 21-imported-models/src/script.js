a9d5881e81a14a2c8022af8e03e3db52c7552a4e
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-25 10:58:50 +0545

    added model

--- 21-imported-models/src/script.js
+++ 21-imported-models/src/script.js
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as dat from 'dat.gui'
 
 /**
@@ -14,6 +15,24 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+gltfLoader.load(
+    '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    (gltf) => {
+        // const children = [...gltf.scene.children]
+        // for (const child of children)
+        // {
+        //     scene.add(child)
+        // }
+        scene.add(gltf.scene)
+    },
+)
+
 
 /**
  * Floor
