de708321d149be43a1c5bb1ab2a249355bf9fe01
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-28 17:02:41 +0545

    animated Galaxy Final

--- 27-animated-galaxy/src/script.js
+++ 27-animated-galaxy/src/script.js
@@ -27,7 +27,7 @@ parameters.size = 0.005
 parameters.radius = 5
 parameters.branches = 3
 parameters.spin = 1
-parameters.randomness = 0.5
+parameters.randomness = 0.2
 parameters.randomnessPower = 3
 parameters.insideColor = '#ff6030'
 parameters.outsideColor = '#1b3984'
@@ -52,7 +52,8 @@ const generateGalaxy = () =>
 
     const positions = new Float32Array(parameters.count * 3)
     const colors = new Float32Array(parameters.count * 3)
-    const scales = new Float32Array(parameters.count)
+    const scales = new Float32Array(parameters.count * 1)
+    const randomness = new Float32Array(parameters.count * 3)
 
     const insideColor = new THREE.Color(parameters.insideColor)
     const outsideColor = new THREE.Color(parameters.outsideColor)
@@ -66,13 +67,18 @@ const generateGalaxy = () =>
 
         const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
 
+        positions[i3    ] = Math.cos(branchAngle) * radius 
+        positions[i3 + 1] = 0.0
+        positions[i3 + 2] = Math.sin(branchAngle) * radius 
+
+        // Randomness
         const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
         const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
         const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
 
-        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
+        randomness[i3] = randomX
+        randomness[i3 + 1] = randomY
+        randomness[i3 + 2] = randomZ
 
         // Scale
         scales[i] = Math.random()
@@ -89,6 +95,7 @@ const generateGalaxy = () =>
     geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
     geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
     geometry.setAttribute('ascale', new THREE.BufferAttribute(colors, 1))
+    geometry.setAttribute('aRandomness', new THREE.BufferAttribute(randomness, 3))
 
     /**
      * Material
@@ -100,8 +107,8 @@ const generateGalaxy = () =>
         vertexShader: galaxyVertexShader,
         fragmentShader: galaxyFragmentShader,
         uniforms: {
-            uSize: { value: 8 * renderer.getPixelRatio()},
-            
+            uSize: { value: 30 * renderer.getPixelRatio()},
+            uTime: { value: 0 }
         }
     })
 
@@ -178,6 +185,9 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Update Materials
+    material.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
