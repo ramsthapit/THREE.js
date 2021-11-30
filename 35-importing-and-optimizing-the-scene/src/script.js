f073c2bc27e75094adbef7fac602ec4c71747043
Ram Sthapit <ramsthapit50@gmail.com>
2021-11-30 17:35:55 +0545

    background color added

--- 35-importing-and-optimizing-the-scene/src/script.js
+++ 35-importing-and-optimizing-the-scene/src/script.js
@@ -17,6 +17,7 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
  * Base
  */
 // Debug
+const debugObject = {}
 const gui = new dat.GUI({
     width: 400
 })
@@ -130,6 +131,14 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.outputEncoding = THREE.sRGBEncoding
+
+debugObject.clearColor = '#201919'
+renderer.setClearColor(debugObject.clearColor)
+gui
+    .addColor(debugObject, 'clearColor')
+    .onChange(() => {
+        renderer.setClearColor(debugObject.clearColor)
+    })
 
 /**
  * Animate
